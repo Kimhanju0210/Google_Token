fab2be7ed6360d43eabaab5427b77c530d36b768
김한주 <s24043@gsm.hs.kr>
2024-12-19 23:03:34 +0900

    original

---
+++ src/main/java/org/example/ntoken/NTokenApplication.java
@@ -0,0 +1,13 @@
+package org.example.ntoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ntoken/NTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ntoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
