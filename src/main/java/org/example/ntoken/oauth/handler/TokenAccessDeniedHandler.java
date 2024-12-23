7f0377adabb0f7713b29e9d835f2a0d210620549
김한주 <s24043@gsm.hs.kr>
2024-12-23 22:48:39 +0900

    add :: TokenAccessDeniedHandler.java

---
+++ src/main/java/org/example/ntoken/oauth/handler/TokenAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package org.example.ntoken.oauth.handler;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+
+@Component
+@RequiredArgsConstructor
+public class TokenAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        handlerExceptionResolver.resolveException(request, response, null, accessDeniedException);
+    }
+
+}
