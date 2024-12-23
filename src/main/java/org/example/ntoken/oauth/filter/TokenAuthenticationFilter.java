00259576c82b3152792bb7a77b4a36470258ba4a
김한주 <s24043@gsm.hs.kr>
2024-12-23 22:46:26 +0900

    add :: TokenAuthenticationFilter.java

---
+++ src/main/java/org/example/ntoken/oauth/filter/TokenAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package org.example.ntoken.oauth.filter;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.ntoken.utils.HeaderUtil;
+import org.example.ntoken.oauth.token.AuthToken;
+import org.example.ntoken.oauth.token.AuthTokenProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    private final AuthTokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String tokenStr = HeaderUtil.getAccessToken(request);
+        AuthToken token = tokenProvider.convertAuthToken(tokenStr);
+
+        if (token.validate()) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
