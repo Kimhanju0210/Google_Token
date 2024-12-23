13c3deae564e0bed9f0080ae4b4c7ec345de889f
김한주 <s24043@gsm.hs.kr>
2024-12-23 22:55:39 +0900

    delete :: All

--- src/main/java/org/example/ntoken/domain/auth/info/OAuth2UserInfo.java
+++
@@ -1,8 +0,0 @@
-package org.example.ntoken.domain.auth.info;
-
-public interface OAuth2UserInfo {
-    String getProvider();
-    String getProviderId();
-    String getEmail();
-    String getName();
-}
