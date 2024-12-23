1047ae328951ab0c4e796cabd219a4b63f340098
김한주 <s24043@gsm.hs.kr>
2024-12-23 22:30:16 +0900

    add :: UserRefreshTokenRepository.java

---
+++ src/main/java/org/example/ntoken/api/repository/user/UserRefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package org.example.ntoken.api.repository.user;
+
+import org.example.ntoken.api.entity.user.UserRefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
+    UserRefreshToken findByUserId(String userId);
+    UserRefreshToken findByRefreshToken(String refreshToken);
+}
