bc8ed0f13b1d29e60497c158bbf211c78fd2604e
김한주 <s24043@gsm.hs.kr>
2024-12-23 22:47:27 +0900

    add :: OAuth2AuthenticationFailureHandler.java

---
+++ src/main/java/org/example/ntoken/oauth/handler/OAuth2AuthenticationFailureHandler.java
@@ -0,0 +1,39 @@
+package org.example.ntoken.oauth.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.ntoken.oauth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import org.example.ntoken.utils.CookieUtil;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.naming.AuthenticationException;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String targetUrl = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAM)
+                .map(Cookie::getValue)
+                .orElse(("/"));
+
+        exception.printStackTrace();
+
+        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("error", exception.getLocalizedMessage())
+                .build().toUriString();
+
+        authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+}
