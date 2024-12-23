b7ef6ad6be4fc803a7dcab513395adca5a238276
김한주 <s24043@gsm.hs.kr>
2024-12-23 22:19:06 +0900

    add :: AuthController.java

---
+++ src/main/java/org/example/ntoken/api/controller/auth/AuthController.java
@@ -0,0 +1,141 @@
+package org.example.ntoken.api.controller.auth;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.ntoken.api.entity.auth.AuthReqModel;
+import org.example.ntoken.common.ApiResponse;
+import org.example.ntoken.config.properties.AppProperties;
+import org.example.ntoken.oauth.entity.UserPrincipal;
+import org.example.ntoken.oauth.entity.RoleType;
+import org.example.ntoken.api.entity.user.UserRefreshToken;
+import org.example.ntoken.api.repository.user.UserRefreshTokenRepository;
+import org.example.ntoken.oauth.token.AuthToken;
+import org.example.ntoken.oauth.token.AuthTokenProvider;
+import org.example.ntoken.utils.CookieUtil;
+import org.example.ntoken.utils.HeaderUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AppProperties appProperties;
+    private final AuthTokenProvider tokenProvider;
+    private final AuthenticationManager authenticationManager;
+    private final UserRefreshTokenRepository userRefreshTokenRepository;
+
+    private final static long THREE_DAYS_MSEC = 259200000;
+    private final static String REFRESH_TOKEN = "refresh_token";
+
+    @PostMapping("/login")
+    public ApiResponse login(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestBody AuthReqModel authReqModel
+    ) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authReqModel.getId(),
+                        authReqModel.getPassword()
+                )
+        );
+
+        String userId = authReqModel.getId();
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        Date now = new Date();
+        AuthToken accessToken = tokenProvider.createAuthToken(
+                userId,
+                ((UserPrincipal) authentication.getPrincipal()).getRoleType().getCode(),
+                new Date(now.getTime() + appProperties.getAuth().getTokenExpiry())
+        );
+
+        long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpiry();
+        AuthToken refreshToken = tokenProvider.createAuthToken(
+                appProperties.getAuth().getTokenSecret(),
+                new Date(now.getTime() + refreshTokenExpiry)
+        );
+
+        UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUserId(userId);
+        if (userRefreshToken == null) {
+            userRefreshToken = new UserRefreshToken(userId, refreshToken.getToken());
+            userRefreshTokenRepository.saveAndFlush(userRefreshToken);
+        } else {
+            userRefreshToken.setRefreshToken(refreshToken.getToken());
+        }
+
+        int cookieMaxAge = (int) refreshTokenExpiry / 60;
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
+        CookieUtil.addCookie(response, REFRESH_TOKEN, refreshToken.getToken(), cookieMaxAge);
+
+        return ApiResponse.success("token", accessToken.getToken());
+    }
+
+    @GetMapping("/refresh")
+    public ApiResponse refreshToken (HttpServletRequest request, HttpServletResponse response) {
+        String accessToken = HeaderUtil.getAccessToken(request);
+        AuthToken authToken = tokenProvider.convertAuthToken(accessToken);
+        if (!authToken.validate()) {
+            return ApiResponse.invalidAccessToken();
+        }
+
+        Claims claims = authToken.getExpiredTokenClaims();
+        if (claims == null) {
+            return ApiResponse.notExpiredTokenYet();
+        }
+
+        String userId = claims.getSubject();
+        RoleType roleType = RoleType.of(claims.get("role", String.class));
+
+        String refreshToken = CookieUtil.getCookie(request, REFRESH_TOKEN)
+                .map(Cookie::getValue)
+                .orElse((null));
+        AuthToken authRefreshToken = tokenProvider.convertAuthToken(refreshToken);
+
+        if (authRefreshToken.validate()) {
+            return ApiResponse.invalidRefreshToken();
+        }
+
+        UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUserIdAndRefreshToken(userId, refreshToken);
+        if (userRefreshToken == null) {
+            return ApiResponse.invalidRefreshToken();
+        }
+
+        Date now = new Date();
+        AuthToken newAccessToken = tokenProvider.createAuthToken(
+                userId,
+                roleType.getCode(),
+                new Date(now.getTime() + appProperties.getAuth().getTokenExpiry())
+        );
+
+        long validTime = authRefreshToken.getTokenClaims().getExpiration().getTime() - now.getTime();
+
+        if (validTime <= THREE_DAYS_MSEC) {
+            long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpiry();
+
+            authRefreshToken = tokenProvider.createAuthToken(
+                    appProperties.getAuth().getTokenSecret(),
+                    new Date(now.getTime() + refreshTokenExpiry)
+            );
+
+            userRefreshToken.setRefreshToken(authRefreshToken.getToken());
+
+            int cookieMaxAge = (int) refreshTokenExpiry / 60;
+            CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
+            CookieUtil.addCookie(response, REFRESH_TOKEN, authRefreshToken.getToken(), cookieMaxAge);
+        }
+
+        return ApiResponse.success("token", newAccessToken.getToken());
+    }
+}
